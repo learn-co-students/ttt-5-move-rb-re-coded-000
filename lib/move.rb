c05259507b02baf182ff8e29b31a8f2f257c95bf
MOHAMMAD DAYLAKI <mohammad.daylaky@gmail.com>
2016-10-03 09:44:56 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -5,5 +5,10 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+def input_to_index(position)
+  position.to_i- 1
+end
 
-# code your move method here!
+def move(board, position, player="X")
+  board[position.to_i- 1]= player
+end
